4b5286ed0c7cdf306d45dcf35a155a26b4ee3e80
Danillo Souza <danillosouza1704@gmail.com>
2022-12-26 22:25:31 -0300

    add: Criado componente botão

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,43 @@
+import { StyleSheet, TextStyle, ViewStyle } from "react-native";
+import { COLORS } from "_/constants/colors";
+import { FONTS } from "_/constants/fonts";
+import { SIZES } from "_/constants/sizes";
+
+const MIN_WIDTH = 180;
+
+type ButtonStyles = {
+  buttonBody: ViewStyle;
+  title: TextStyle;
+};
+
+export const baseStyles = StyleSheet.create<ButtonStyles>({
+  buttonBody: {
+    borderRadius: SIZES.RADIUS_SMALL,
+    minWidth: MIN_WIDTH,
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: FONTS.FONT_SIZE_REGULAR,
+    paddingVertical: SIZES.PADDING_LARGE,
+    paddingHorizontal: SIZES.PADDING_XX_LARGE,
+    fontFamily: FONTS.INTER_FAMILY,
+  },
+});
+
+export const primaryStyles = StyleSheet.create<ButtonStyles>({
+  buttonBody: {
+    backgroundColor: COLORS.PRIMARY_COLOR,
+  },
+  title: {
+    color: COLORS.WHITE,
+  },
+});
+
+export const secondaryStyles = StyleSheet.create<ButtonStyles>({
+  buttonBody: {
+    backgroundColor: COLORS.WHITE,
+  },
+  title: {
+    color: COLORS.BLACK,
+  },
+});
