e7356577fcb0de6900b39bc28bc2ae04a1aa0c21
Danillo Souza <danillosouza1704@gmail.com>
2023-01-04 09:06:54 -0300

    add: Fonte Nonato

--- src/constants/texts.ts
+++ src/constants/texts.ts
@@ -1,3 +1,5 @@
 export const TEXTS = {
   MORE_INFO: "Mais Informações",
+  WELCOME:
+    "Acompanhe ou cadastre totens para poder visualizar a qualidade de ar de uma região.",
 };
--- src/hooks/useFonts.ts
+++ src/hooks/useFonts.ts
@@ -1,16 +1,22 @@
 import { useFonts as useFontsExpo } from "expo-font";
 
+const fontsAvailable = {
+  InterBlack: require("_/assets/fonts/Inter-Black.ttf"),
+  InterBold: require("_/assets/fonts/Inter-Bold.ttf"),
+  InterExtraBold: require("_/assets/fonts/Inter-ExtraBold.ttf"),
+  InterExtraLight: require("_/assets/fonts/Inter-ExtraLight.ttf"),
+  InterMedium: require("_/assets/fonts/Inter-Medium.ttf"),
+  InterRegular: require("_/assets/fonts/Inter-Regular.ttf"),
+  InterSemiBold: require("_/assets/fonts/Inter-SemiBold.ttf"),
+  InterThin: require("_/assets/fonts/Inter-Thin.ttf"),
+  NunitoBold: require("_/assets/fonts/Nunito-Bold.ttf"),
+  NunitoRegular: require("_/assets/fonts/Nunito-Regular.ttf"),
+};
+
+export type FontsAvailableType = keyof typeof fontsAvailable;
+
 export const useFonts = (): boolean => {
-  const [isFontsLoaded] = useFontsExpo({
-    InterBlack: require("_/assets/fonts/Inter-Black.ttf"),
-    InterBold: require("_/assets/fonts/Inter-Bold.ttf"),
-    InterExtraBold: require("_/assets/fonts/Inter-ExtraBold.ttf"),
-    InterExtraLight: require("_/assets/fonts/Inter-ExtraLight.ttf"),
-    InterMedium: require("_/assets/fonts/Inter-Medium.ttf"),
-    InterRegular: require("_/assets/fonts/Inter-Regular.ttf"),
-    InterSemiBold: require("_/assets/fonts/Inter-SemiBold.ttf"),
-    InterThin: require("_/assets/fonts/Inter-Thin.ttf"),
-  });
+  const [isFontsLoaded] = useFontsExpo(fontsAvailable);
 
   return isFontsLoaded;
 };
