6d42975a92a719d4508f4ec145966bed806000b3
micaellagouveia <micaella2212@gmail.com>
2023-01-16 18:23:13 -0300

    feat: add map on detail totem screen

--- src/screens/TotemScreen/index.tsx
+++ src/screens/TotemScreen/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { View, ViewStyle } from "react-native";
-import { Button, Text } from "_/components";
-import { homeImage } from "_/assets/home-image";
-import { SvgXml } from "react-native-svg";
-import { TEXTS } from "_/constants/texts";
-import { COLORS } from "_/constants/colors";
+import { Text } from "_/components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { AirQualityCard } from '_/components';
+import MapView from "react-native-maps";
 
 import styles from "./styles";
 
@@ -17,22 +14,51 @@ const TotemScreen = () => {
 
   return (
     <View style={[styles.container, safeArea]}>
-      <View>
+      <View style={[styles.resumeWrapper]}>
         <Text family="InterBold" size="large" style={styles.totemTitle}>
           Totem 1
         </Text>
-        <Text family="InterBold" size="regular" style={styles.resumText}>
+        <Text family="InterBold" size="regular" style={styles.resumeText}>
           Resumo
         </Text>
+        <View style={styles.mapWrapper}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -15.98961, //será as coordenadas do totem
+              longitude: -48.0443975,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
       </View>
-      <AirQualityCard
-        dataCollected={30}
-        minMaxValues={{ min: 15, max: 40 }}
-        titleProps={{ title: 'Temperatura', iconName: 'device-thermostat' }}
-        dataType="%"
-        />
       <View>
-
+        <View style={[styles.cards]}>
+          <AirQualityCard
+            dataCollected={30}
+            minMaxValues={{ min: 15, max: 40 }}
+            titleProps={{ title: 'Temperatura', iconName: 'device-thermostat' }}
+            dataType="%"
+          />
+          <AirQualityCard
+            dataCollected={90}
+            minMaxValues={{ min: 60, max: 95 }}
+            titleProps={{ title: 'Umidade', iconName: 'cloud' }}
+            dataType="%"
+          />
+        </View>
+        <View style={[styles.cards]}>
+          <AirQualityCard
+            dataCollected={65}
+            minMaxValues={{ min: 30, max: 90 }}
+            titleProps={{ title: 'Qualidade do ar', iconName: 'attractions' }}
+            dataType="%"
+          />
+        </View>
+        <Text family="InterRegular" size="regular" style={styles.updateText}>
+          Dados atualizados em: 15/11/2022 12:15
+        </Text>
       </View>
     </View>
   );
--- src/screens/TotemScreen/styles.ts
+++ src/screens/TotemScreen/styles.ts
@@ -1,30 +1,42 @@
 import { StyleSheet } from "react-native";
 import { COLORS } from "_/constants/colors";
 import { SIZES } from "_/constants/sizes";
+import { FONTS } from "_/constants/fonts";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: SIZES.PADDING_XX_LARGE,
     backgroundColor: COLORS.GREY_PRIMARY,
+  },
+  resumeWrapper: {
+    marginBottom: SIZES.MARGING_XX_LARGE,
+  },
+  cards: {
+    flexDirection: "row",
     justifyContent: "space-between",
+    marginVertical: SIZES.MARGING_REGULAR,
+
   },
-  baseText: {
-    textAlign: "center",
+  mapWrapper: {
+    marginTop: SIZES.MARGING_SMALL,
+    justifyContent: 'center',
   },
-  image: {
-    marginVertical: SIZES.MARGING_XX_LARGE,
+  map: {
+    width: '100%',
+    height: 150,
   },
   totemTitle: {
-    // marginBottom: SIZES.MARGING_X_SMALL,
     textAlign: "center",
   },
-  resumText: {
-    // marginBottom: SIZES.MARGING_X_SMALL,
+  resumeText: {
+    marginBottom: SIZES.MARGING_X_SMALL,
+    fontSize: FONTS.FONT_SIZE_REGULAR,
   },
-  mainButton: {
-    marginBottom: SIZES.MARGING_LARGE,
-  },
+  updateText: {
+    color: COLORS.GREY_TERTIARY,
+    fontSize: FONTS.FONT_SIZE_SMALL,
+  }
 });
 
 export default styles;
