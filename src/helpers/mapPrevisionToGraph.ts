71930dc8dc83d964b79c746ff0717f641d13d48b
sudjoao <isudjoao@gmail.com>
2023-02-09 23:17:15 -0300

    feat: show graph with prevision

---
+++ src/helpers/mapPrevisionToGraph.ts
@@ -0,0 +1,14 @@
+import { GraphValues } from "_/screens/ChartsScreen";
+import { PrevisionDto } from "_/types/dto/prevision";
+
+export const mapPrevisionsToGraph = (
+  prevision: PrevisionDto
+): GraphValues[] => {
+  const date = new Date()
+  const _previsions = prevision.previsions.map((value, i) => {
+    date.setHours(date.getHours()+1)
+    return { x: date.toISOString(), y: value };
+  });
+
+  return _previsions as GraphValues[];
+};
